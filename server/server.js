3a54652eb92d87b63f606328192c0b9b69dcb023
Jorge Mendoza <jorge.mendoza@e-openroad.com>
2020-08-25 18:17:53 -0500

    Restserver - usuario

--- server/server.js
+++ server/server.js
@@ -1,45 +1,19 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
-    res.json({ 'name': 'Jorge' });
-});
+app.use(require('./routes/usuario'));
 
-app.get('/usuario', function(req, res) {
-    res.json({ 'name': 'Jorge' });
-});
-
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre el necesario'
-        });
-    } else {
-        res.json({ persona: body });
-    }
-
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({ id });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json({ 'name': 'Jorge' });
-});
+mongoose.connect(process.env.URLDB, {},
+    () => { console.log('Database Online'); },
+    err => { console.log(err); });
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando en el puerto 3000');
