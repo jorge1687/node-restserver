926938db23c921f96b1b5941092c9a13cbd972eb
Jorge Mendoza <jorge.mendoza@e-openroad.com>
2020-08-26 00:08:50 -0500

    Uno mas

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,9 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = process.env.MONGO_URI;
+    //urlDB = process.env.MONGO_URI;
+
+    urlDB = 'mongodb+srv://jorge:jorge@mendoza-m0.7mjte.mongodb.net/cafe?retryWrites=true&w=majority';
 }
 process.env.URLDB = urlDB;
 console.log('----->' + process.env.URLDB);
